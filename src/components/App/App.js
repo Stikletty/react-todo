789bfd7aca37ed1f27862432abba28332522a435
Gabor Troll <gabor.troll@gekko-marketing.com>
2021-02-22 17:57:06 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="todoApp">
+        <h1>TodoApp</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
